8e669c1b92a106fe6a55b480ee6f0e7d13156d3d
sianida26 <chesakeselekkebo@gmail.com>
2023-01-22 05:16:52 +0700

    switch to scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import './styles/bootstrap.scss';
 import './styles/style.css'
 
 import App from './App'
