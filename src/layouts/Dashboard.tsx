7e9234cea1e2bbef35aaaa87de96848e9e007c65
sianida26 <chesakeselekkebo@gmail.com>
2023-01-22 05:28:15 +0700

    added type fee index page

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,12 +1,20 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
+import Container from "react-bootstrap/Container";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
 
 export default function Dashboard() {
 	return (
 		<div>
 			<Navbar />
-			<Outlet />
+            <Container className="mt-4">
+                <Breadcrumb className="text-xs">
+                    <Breadcrumb.Item>Master Data Management</Breadcrumb.Item>
+                    <Breadcrumb.Item active>Fee Type</Breadcrumb.Item>
+                </Breadcrumb>
+			    <Outlet />
+            </Container>
 		</div>
 	);
 }
--- src/pages/TypeFeePage.tsx
+++ src/pages/TypeFeePage.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function TypeFeePage() {
   return (
-    <div>TypeFeePage</div>
+    <main>
+        <h2>Fee Type</h2>
+    </main>
   )
 }
