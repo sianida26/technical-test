136c540b1b890303ec9c157152d251184731ffbd
sianida26 <chesakeselekkebo@gmail.com>
2023-01-22 06:29:14 +0700

    Added datatable

--- src/pages/TypeFeePage.tsx
+++ src/pages/TypeFeePage.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
+import $ from "jquery";
+import "datatables.net";
+ 
 export default function TypeFeePage() {
-  return (
-    <main>
-        <h2>Fee Type</h2>
-    </main>
-  )
+	const data = [
+		[
+			"Tiger Nixon",
+			"System Architect",
+			"Edinburgh",
+			"5421",
+			"2011/04/25",
+			"$320,800",
+		],
+		[
+			"Garrett Winters",
+			"Accountant",
+			"Tokyo",
+			"8422",
+			"2011/07/25",
+			"$170,750",
+		],
+		[
+			"Ashton Cox",
+			"Junior Technical Author",
+			"San Francisco",
+			"1562",
+			"2009/01/12",
+			"$86,000",
+		],
+	];
+	const tableRef = useRef<HTMLTableElement>(null);
+	const table = useRef<any>(null);
+    const [ a, setA ] = useState(false)
+	useEffect(() => {
+		if (tableRef.current) {
+			const table = $(tableRef.current).DataTable({
+				data: data,
+				columns: [
+					{ title: "Name" },
+					{ title: "Position" },
+					{ title: "Office" },
+					{ title: "Extn." },
+					{ title: "Start date" },
+					{ title: "Salary" },
+				],
+			});
+			return () => { table.destroy(false) }
+		}
+	}, []);
+
+	useEffect(() => {
+		if (table.current) {
+			table.current.destroy();
+		}
+	}, []);
+
+	return (
+		<main>
+			<h2>Fee Type</h2>
+			<table className="display" width="100%" ref={tableRef}></table>
+		</main>
+	);
 }
